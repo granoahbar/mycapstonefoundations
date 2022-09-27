71e9cb4c95669a31cf5ccd148b3adddcbfcb9ad8
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-09-27 17:23:34 -0600

    db seeded

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-app.post('/seed', seed)
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const {SERVER_PORT} = process.env
+const {seed} = require('./seed.js')
 const {} = require('./controller.js')
 
 app.use(express.json())
 app.use(cors())
+
+app.post('/seed', seed)
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
--- server/seed.js
+++ server/seed.js
@@ -1,5 +1,7 @@
-require('dotnev').config()
+require('dotenv').config()
 const {CONNECTION_STRING} = process.env;
+
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(CONNECTION_STRING, {
     dialect: 'postgres',
@@ -9,4 +11,31 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
         }
     }
  })
+
+ module.exports = {
+    seed: (req, res) => {
+        sequelize.query(`
+        
+        create table friends (
+            friend_id serial primary key,
+            first_name varchar(150),
+            last_name varchar(150),
+            notes text
+        );
+       
+        create table events (
+            event_id serial primary key,
+            friend_id integer references friends(friend_id),
+            event_name varchar(50),
+            event_date date,
+            event_time time
+        );
+    
+        `).then(() => {
+            console.log('DB seeded!')
+            res.sendStatus(200)
+        }).catch(err => console.log('error seeding DB', err))
+    }
+}
+ 
  
